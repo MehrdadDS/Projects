aeaf90ab5c1bbf710d622d6c8cfdab8279058e6e
Mehrdad <mehrdad.dadgar@gmail.com>
2024-05-15 10:28:50 -0400

    minor change

--- TelegramBot/message_handler.py
+++ TelegramBot/message_handler.py
@@ -7,4 +7,16 @@ bot = telebot.TeleBot('..')
 def welcome(message):
     bot.send_message(message.chat.id,'welcome to Mehrdad bot.')
     bot.reply_to(message,'This is a reply')
+    bot.register_next_step_handler(message,process_name)
+
+def process_name(message):
+    name = message.text
+    bot.send_message(message.chat.id,f"Hello {name}! this is going to be a test")
+
+    bot.register_next_step_handler(message,process_age)
+
+def process_age(message):
+    age = message.text
+    bot.send_message(message.chat.id,f"you are {age} years old.")
 bot.polling()
+

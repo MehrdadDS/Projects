00a85f0bacfe1048acbc3923b39751da26c7d509
Mehrdad <mehrdad.dadgar@gmail.com>
2024-05-28 22:31:33 -0400

    commit the codes

--- Bots/IBKRBot/IBAPI/Mehrdad Bot/Strategeies.py
+++ Bots/IBKRBot/IBAPI/Mehrdad Bot/Strategeies.py
@@ -3,14 +3,15 @@ import numpy as np
 import logging
 
 # Configure logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+#logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class TradingStrategies:
     higher_time_frame = {
         '15 mins': '1 hour',
         '1 hour': '4 hours',
         '4 hours': '1 day',
-        '1 day': '1 week'
+        '1 day': '1 week',
+        '1 week':'1 month'
     }
 
     def __init__(self, df):
@@ -35,7 +36,7 @@ class TradingStrategies:
             df = self.df[self.df["Time Frame"] == time_frame]
             ticker = df['Ticker'][0]
             if df.empty:
-                logging.warning(f"No data found for the time frame: {time_frame}")
+                print(f"No data found for the time frame: {time_frame}")
                 return trade_trigger, time_frame, None, None, None
 
             latest_close = df["Close"].iloc[-1]
@@ -46,45 +47,39 @@ class TradingStrategies:
             tunnel_ema_low = df["Tunnel_EMA_Low"].iloc[-1]
 
             target_time_frame = self.higher_time_frame.get(time_frame)
-            if target_time_frame is None:
-                logging.error(f"No higher time frame found for: {time_frame}")
-                return trade_trigger, time_frame, None, None, None
+            if target_time_frame is not None:
+                #logging.error(f"No higher time frame found for: {time_frame}")
+                target = db[db['Time Frame'] == target_time_frame]["Tunnel_EMA_Low"].iloc[-1]
 
-            target = db[db['Time Frame'] == target_time_frame]["Tunnel_EMA_Low"].iloc[-1]
+                if (latest_close > ema_12_filter and
+                    ema_12_filter > wave_ema_high and
+                    ema_12_filter > tunnel_ema_high and
+                    latest_close < target * 0.99):
 
-            if (latest_close > ema_12_filter and
-                ema_12_filter > wave_ema_high and
-                ema_12_filter > tunnel_ema_high and
-                latest_close < target * 0.99):
+                    stoploss = min(wave_ema_low, tunnel_ema_low)
+                    entry_point = latest_close
+                    trade_trigger = 'Yes'
+                    potential_profit = np.round((target / entry_point) - 1, 2) * 100
+                    potential_loss = np.round((entry_point / stoploss) - 1, 2) * 100
 
-                stoploss = min(wave_ema_low, tunnel_ema_low)
-                entry_point = latest_close
-                trade_trigger = 'Yes'
-                potential_profit = np.round((target / entry_point) - 1, 2) * 100
-                potential_loss = np.round((entry_point / stoploss) - 1, 2) * 100
+                    print(f'Trigger received in {time_frame} time frame:\n- Entry point: {entry_point}$\n- Stoploss: {stoploss}$\n- Target: {target}$\n- Risk/Reward: {potential_profit / potential_loss}\n- Potential Loss: {potential_loss}%\n- Potential Profit: {potential_profit}%')
 
-                logging.info(f'Trigger received in {time_frame} time frame:'
-                             f'\n- Entry point: {entry_point}$'
-                             f'\n- Stoploss: {stoploss}$'
-                             f'\n- Target: {target}$'
-                             f'\n- Risk/Reward: {potential_profit / potential_loss}'
-                             f'\n- Potential Loss: {potential_loss}%'
-                             f'\n- Potential Profit: {potential_profit}%')
-
-                return {'ticker':ticker,'trade_trigger':trade_trigger, 'time_frame':time_frame, 'entry_point':entry_point, 'stoploss':stoploss, 'target':target,'Risk/Reward':potential_profit / potential_loss}
+                    return {'ticker':ticker,'trade_trigger':trade_trigger, 'time_frame':time_frame, 'entry_point':entry_point, 'stoploss':stoploss, 'target':target,'Risk/Reward':potential_profit / potential_loss}
+                else:
+                    print('No setup found')
+                    return {'ticker':ticker,'trade_trigger':trade_trigger, 'time_frame':time_frame, 'entry_point':None, 'stoploss':None, 'target':None,'Risk/Reward':None}
             else:
-                logging.info('No setup found')
-                return {'ticker':ticker,'trade_trigger':trade_trigger, 'time_frame':time_frame, 'entry_point':None, 'stoploss':None, 'target':None,'Risk/Reward':None}
-
+                    print(f"No higher time frame found for: {time_frame}")
+                    return trade_trigger, time_frame, None, None, None
         except KeyError as e:
-            logging.error(f"KeyError: {e}")
-            return {'ticker':ticker,'trade_trigger':trade_trigger, 'time_frame':time_frame, 'entry_point':None, 'stoploss':None, 'target':None,'Risk/Reward':None}
-        except IndexError as e:
-            logging.error(f"IndexError: {e}")
-            return {'ticker':ticker,'trade_trigger':trade_trigger, 'time_frame':time_frame, 'entry_point':None, 'stoploss':None, 'target':None,'Risk/Reward':None}
-        except Exception as e:
-            logging.error(f"An unexpected error occurred: {e}")
-            return {'ticker':ticker,'trade_trigger':trade_trigger, 'time_frame':time_frame, 'entry_point':None, 'stoploss':None, 'target':None,'Risk/Reward':None}
+            print(f"KeyError: {e}")
+        #    return {'ticker':ticker,'trade_trigger':trade_trigger, 'time_frame':time_frame, 'entry_point':None, 'stoploss':None, 'target':None,'Risk/Reward':None}
+        #except IndexError as e:
+            #logging.error(f"IndexError: {e}")
+        #    return {'ticker':ticker,'trade_trigger':trade_trigger, 'time_frame':time_frame, 'entry_point':None, 'stoploss':None, 'target':None,'Risk/Reward':None}
+        #except Exception as e:
+            #logging.error(f"An unexpected error occurred: {e}")
+        #    return {'ticker':ticker,'trade_trigger':trade_trigger, 'time_frame':time_frame, 'entry_point':None, 'stoploss':None, 'target':None,'Risk/Reward':None}
 
 
 
@@ -95,7 +90,7 @@ class TradingStrategies:
             df = self.df[self.df["Time Frame"] == time_frame]
             ticker = df['Ticker'][0]
 
-            return {'ticker':ticker,'trade_trigger':trade_trigger, 'time_frame':time_frame, 'entry_point':20, 'stoploss':15, 'target':40}
+            return {'ticker':ticker,'trade_trigger':trade_trigger, 'time_frame':time_frame, 'entry_point':1, 'stoploss':0.5, 'target':2}
 
 # Example usage:
 # df = pd.read_csv('your_data.csv')  # Load your data into a DataFrame
---
+++ Bots/IBKRBot/ib_order_basic stop_order.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+
+"""
+
+
+from ibapi.client import EClient
+from ibapi.wrapper import EWrapper
+from ibapi.contract import Contract
+from ibapi.order import Order
+import threading
+import time
+
+
+class TradingApp(EWrapper, EClient):
+    def __init__(self):
+        EClient.__init__(self,self)
+        
+    def error(self, reqId, errorCode, errorString):
+        print("Error {} {} {}".format(reqId,errorCode,errorString))
+        
+    def nextValidId(self, orderId):
+        super().nextValidId(orderId)
+        self.nextValidOrderId = orderId
+        print("NextValidId:", orderId)
+
+def websocket_con():
+    app.run()
+    
+app = TradingApp()
+app.connect("127.0.0.1", 7496, clientId=1)
+
+# Starting a separate daemon thread to execute the websocket connection
+con_thread = threading.Thread(target=websocket_con, daemon=True)
+con_thread.start()
+
+# Adding some latency to ensure that the connection is established
+time.sleep(1)
+
+# Creating an object of the Contract class - will be used as a parameter for other function calls
+contract = Contract()
+contract.symbol = "AAPL"
+contract.secType = "STK"
+contract.currency = "USD"
+contract.exchange = "SMART"
+
+# Creating an object of the Order class - will be used as a parameter for other function calls
+order = Order()
+order.action = "BUY"
+order.orderType = "STP LMT"
+order.totalQuantity = 1
+order.lmtPrice = 190
+order.auxPrice = 185
+order.eTradeOnly = ""
+order.firmQuoteOnly = ""
+# Ensure nextValidOrderId is set before placing the order
+while app.nextValidOrderId is None:
+    print("Waiting for nextValidOrderId...")
+    time.sleep(1)
+
+app.placeOrder(app.nextValidOrderId, contract, order)  # EClient function to place order
+#app.cancelOrder(5)
+time.sleep(5)  # Adding some latency to ensure that the order is placed

6e1966723ade383d3a2d1c13c9b0350f3fa51907
Mehrdad <mehrdad.dadgar@gmail.com>
2024-06-08 11:32:40 -0400

    telegrambot improvement

--- Bots/IBKRBot/IBAPI/Mehrdad Bot/telegrambot.py
+++ Bots/IBKRBot/IBAPI/Mehrdad Bot/telegrambot.py
@@ -1,11 +1,30 @@
-import telebot
-bot = telebot.TeleBot('6973724292:AAH4XTP3y1a-6EKi0yFBcqfSR45TsznSMJI')
+# telegrambot.py
+
+import requests
 
 class TelegramBot:
-    def __init__(self):
-        self.bot = telebot.TeleBot('6973724292:AAH4XTP3y1a-6EKi0yFBcqfSR45TsznSMJI')
-    
+    def __init__(self, bot_token, chat_id):
+        self.bot_token = bot_token
+        self.chat_id = chat_id
 
-    def tlg_send_message(message):
-        bot.send_message(83167574, text=message, parse_mode='Markdown')
+    def tlg_send_message(self, message):
+        url = f"https://api.telegram.org/bot{self.bot_token}/sendMessage"
+        data = {
+            "chat_id": self.chat_id,
+            "text": message,
+            "parse_mode": "HTML"
+        }
+        requests.post(url, data=data)
 
+    @staticmethod
+    def format_signal(signal):
+        formatted_message = f"""
+        Ticker: {signal['ticker']}
+        Time Frame: {signal['time_frame']}
+        Trade Trigger: {signal['trade_trigger']}
+        Entry Point: {signal['entry_point']}
+        Stop Loss: {signal['stoploss']}
+        Target: {signal['target']}
+        Risk to Reward: {signal['risk_to_reward']}
+        """
+        return formatted_message

4ed99d30f4b334b56f33a27ed370c864a7530475
Mehrdad <mehrdad.dadgar@gmail.com>
2024-06-27 00:23:58 -0400

    config file

---
+++ Bots/Mehrdad Algo Bot version 3/config.py
@@ -0,0 +1,17 @@
+# config.py
+
+# IB API connection settings
+IB_HOST = "127.0.0.1"
+IB_PORT = 7497
+IB_CLIENT_ID = 1
+
+# Contract details
+CONTRACT_EXCHANGE = "ISLAND"
+CONTRACT_CURRENCY = "USD"
+
+# Telegram bot settings
+TELEGRAM_BOT_TOKEN = "YOUR_BOT_TOKEN"
+TELEGRAM_CHAT_ID = "YOUR_CHAT_ID"
+
+# In config.py
+DB_URL = 'mysql+pymysql://root:123456@localhost/stocks'

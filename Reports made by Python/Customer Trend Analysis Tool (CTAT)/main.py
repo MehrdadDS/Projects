6e631db07f1df61b3ceb303e34bc1c7e128e76c6
Mehrdad <mehrdad.dadgar@gmail.com>
2024-07-10 00:21:21 -0400

    - Taking care of extra weeks - Fixing plot function - Creating a pdf file for the model

--- Reports made by Python/Customer Trend Analysis Tool (CTAT)/main.py
+++ Reports made by Python/Customer Trend Analysis Tool (CTAT)/main.py
@@ -3,25 +3,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pymannkendall as mk
 from functions import process_excel
-last_week_2024 = 27
+from plots import plot_trend_lines
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from matplotlib.backends.backend_pdf import PdfPages
+import pandas as pd
 
+last_week_2024 = 27
+starting_year =2023
+ending_year = 2024
 # Load your dataset
 path = r'Input/your_dataset.xlsx'
 db = process_excel(path)
 
-condition = ((db['Year']<2024)&(db['Year']>=2023)) |((db['Year']==2024) & (db['Week']<=last_week_2024))
+condition = ((db['Year']<ending_year)&(db['Year']>=starting_year)) |((db['Year']==ending_year) & (db['Week']<=last_week_2024))
 db = db[condition]
+db['Master Client'] = db['Master Client'].fillna("No Master Customer")
 
 
 holidays = pd.read_excel(r'C:\My Folder\Forecasts\Holidays_2025.xlsx',sheet_name='Working Days by Week')
 holidays = holidays[['Year', 'Week Number','Working Days']].drop_duplicates().rename(columns= {'Week Number':'Week'})
-db['Master Client'] = db['Master Client'].fillna("No Master Customer")
 
 years = db['Year'].unique()
 weeks = range(1, int(max(db['Week'])))  # Assuming weeks 1 to 52
 master_clients = db['Master Client'].unique()
 complete_weeks = pd.MultiIndex.from_product([years, weeks, master_clients], names=['Year', 'Week', 'Master Client'])
 complete_weeks_df = pd.DataFrame(index=complete_weeks).reset_index()
+condition = ((complete_weeks_df['Year']<ending_year)&(complete_weeks_df['Year']>=starting_year)) |((complete_weeks_df['Year']==ending_year) & (complete_weeks_df['Week']<=last_week_2024))
+complete_weeks_df = complete_weeks_df[condition]
 # Group by and sum
 grouped_db = db.groupby(['Year', 'Week', 'Master Client'])['Pieces'].sum().reset_index()
 
@@ -36,9 +45,13 @@ merged_db['daily_avg'] = round(merged_db['Pieces']/merged_db['Working Days'],1)
 merged_db = merged_db.sort_values(['Master Client','Year','Week'])
 
 customer_total = merged_db.groupby('Master Client')['daily_avg'].mean().reset_index()
-customer_list = sorted(list(customer_total[customer_total['daily_avg']>=5]['Master Client']))
-
+customer_total = customer_total.sort_values('daily_avg',ascending=False)
+customer_list = list(customer_total[customer_total['daily_avg']>=100]['Master Client'])
 result_db = {}
+# Create a PDF object
+pdf_pages = PdfPages("Customer Trend Analysis_2022.pdf")
+
+
 for cus in customer_list:
     print(f'start working on {cus}')
     data = merged_db[merged_db['Master Client'] == cus]
@@ -49,59 +62,21 @@ for cus in customer_list:
     data['Pct_Change'].fillna(0, inplace=True)
 
     # Calculate rolling 4-weeks and 8-weeks
-    data['4Moving_Avg'] = round(data.groupby('Master Client')['Pct_Change'].transform(lambda x: x.rolling(window=4).sum()), 2)
-    data['8Moving_Avg'] = round(data.groupby('Master Client')['Pct_Change'].transform(lambda x: x.rolling(window=8).sum()), 2)
-    data['CumMoving_Avg'] = round(data.groupby('Master Client')['Pct_Change'].cumsum(), 2)
+    #data['4Moving_Avg'] = round(data.groupby('Master Client')['Pct_Change'].transform(lambda x: x.rolling(window=4).sum()), 2)
+    #data['8Moving_Avg'] = round(data.groupby('Master Client')['Pct_Change'].transform(lambda x: x.rolling(window=8).sum()), 2)
+    #data['CumMoving_Avg'] = round(data.groupby('Master Client')['Pct_Change'].cumsum(), 2)
 
     # Mann-Kendall test
     result = mk.original_test(data['daily_avg'])
-
-    """
-    # Plotting the chart
-    plt.figure(figsize=(12, 8))
-    for year in data['Year'].unique():
-        subset = data[data['Year'] == year]
-        plt.plot(subset['Week'], subset['CumMoving_Avg'], marker='o', label='Cumulative')
-        plt.plot(subset['Week'], subset['4Moving_Avg'], marker='o', label='4weeks')
-        plt.plot(subset['Week'], subset['8Moving_Avg'], marker='o', label='8weeks')
-    
-    plt.xlabel('Week')
-    plt.title(f'{cus} - {"Uptrend" if result.trend == "increasing" else "Downtrend"} (p-value: {result.p:.2f})')
-    plt.xticks(data['Week'])
-    plt.title(f'{cus}')
-    plt.legend(title='Year')
-    plt.ylabel("%_change")
-    plt.grid(True)
-    
-    #plt.show()
-
-    # Plotting daily_avg
-    plt.figure(figsize=(12, 8))
-    plt.plot(data['Week'], data['daily_avg'], marker='o')
-    plt.xlabel('Week')
-    plt.xticks(data['Week'])
-    plt.ylabel('daily_avg')
-    plt.grid(True)
-    #plt.show()
-
-    # Additional metric: Rolling standard deviation
-    data['Rolling_Std'] = round(data.groupby('Master Client')['Pct_Change'].transform(lambda x: x.rolling(window=8).std()), 2)
-    
-    # Visualization of rolling standard deviation
-    plt.figure(figsize=(12, 8))
-    plt.plot(data['Week'], data['Rolling_Std'], marker='o', color='purple')
-    plt.xlabel('Week')
-    plt.xticks(data['Week'])
-    plt.ylabel('Rolling Std Dev of % Change')
-    plt.title(f'{cus} - Rolling Std Dev of % Change')
-    plt.grid(True)
-    #plt.show()
-"""
     # Print Mann-Kendall test result
     #print(f"Mann-Kendall Test for {cus}: Trend: {result.trend}, P-value: {result.p:.2f}, Significance: {'Significant' if result.p < 0.05 else 'Not Significant'}")
     result_db[cus] = [result.trend,result.p,'Significant' if result.p < 0.05 else 'Not Significant',round(customer_total[customer_total['Master Client']==cus].iloc[0,1],0)]
+    
+    customer_fig = plot_trend_lines(data, result_db, "Customer Trend Analysis_2022.pdf", last_week_2024)
+    pdf_pages.savefig(customer_fig)
     #print(f'{cus} is done')
 
-
+pdf_pages.close()
 result_db = pd.DataFrame.from_dict(result_db, orient='index',columns=['trend','p_value','Sig_or_NotSig','magnitude of daily_avg'])
-result_db.to_excel('result.xlsx')
+result_db.to_excel('result_2022.xlsx')
+

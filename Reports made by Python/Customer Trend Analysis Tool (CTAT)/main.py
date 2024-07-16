6b6dd70b2386f84538b19faeebd65678d6276366
Mehrdad <mehrdad.dadgar@gmail.com>
2024-07-16 16:48:56 -0400

    update ctat

--- Reports made by Python/Customer Trend Analysis Tool (CTAT)/main.py
+++ Reports made by Python/Customer Trend Analysis Tool (CTAT)/main.py
@@ -44,15 +44,15 @@ merged_db = merged_db.merge(holidays,on=['Year','Week'])
 merged_db['daily_avg'] = round(merged_db['Pieces']/merged_db['Working Days'],1)
 merged_db = merged_db.sort_values(['Master Client','Year','Week'])
 
-customer_total = merged_db.groupby('Master Client')['daily_avg'].mean().reset_index()
+customer_total = merged_db[merged_db['Year']>=2024].groupby('Master Client')['daily_avg'].mean().reset_index()
 customer_total = customer_total.sort_values('daily_avg',ascending=False)
-customer_list = list(customer_total[customer_total['daily_avg']>=100]['Master Client'])
+customer_list = list(customer_total[customer_total['daily_avg']>=50]['Master Client'])
 result_db = {}
 # Create a PDF object
-pdf_pages = PdfPages("Customer Trend Analysis_2022.pdf")
+pdf_pages = PdfPages("Customer Trend Analysis_2023.pdf")
 
 
-for cus in customer_list:
+for cus in customer_list[1:10]:
     print(f'start working on {cus}')
     data = merged_db[merged_db['Master Client'] == cus]
     #data = data[data['Year'] >= 2024]
@@ -72,7 +72,7 @@ for cus in customer_list:
     #print(f"Mann-Kendall Test for {cus}: Trend: {result.trend}, P-value: {result.p:.2f}, Significance: {'Significant' if result.p < 0.05 else 'Not Significant'}")
     result_db[cus] = [result.trend,result.p,'Significant' if result.p < 0.05 else 'Not Significant',round(customer_total[customer_total['Master Client']==cus].iloc[0,1],0)]
     
-    customer_fig = plot_trend_lines(data, result_db, "Customer Trend Analysis_2022.pdf", last_week_2024)
+    customer_fig = plot_trend_lines(data, result_db, "Customer Trend Analysis_2023.pdf", last_week_2024)
     pdf_pages.savefig(customer_fig)
     #print(f'{cus} is done')
 

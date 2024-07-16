d83a140445416ae63a38c66cca94672bfc044efc
Mehrdad <mehrdad.dadgar@gmail.com>
2024-07-16 16:05:01 -0400

    updating peak distribution based on last distribution

--- Forecast Adjustments/Changing Distribution/changing_distribution.py
+++ Forecast Adjustments/Changing Distribution/changing_distribution.py
@@ -16,7 +16,8 @@ from datetime import timedelta
 
 ########################################## Import files
 # Import Forecast
-df = pd.read_excel('C:\My Folder\P&D Forecast\P&D Forecast v2\Excel Output\ForecastResults_onesheet.xlsx',sheet_name='Forecast')
+
+df = pd.read_excel('C:\My Folder\P&D Forecast\P&D Forecast v2\Excel Output\ForecastResults_onesheet_modified.xlsx')
 distributions = pd.read_excel('C:\My Folder\P&D Forecast\P&D Forecast v2\Excel Output\May 7\ForecastResults-Check- April 25th- Checking 2024.xlsm',sheet_name="Actual")
 
 
@@ -31,24 +32,27 @@ distributions.columns = df.columns
 #-----------------------------------------------------------------------------
 #Changing distribution
 distributions = distributions[distributions['Year']==2024]
-adjusting_variables = ['PCL.Del.Pcs.N','PCL.Del.Stops.N','Agent.Del.Pcs.N','Total.Del.Stops.N']
-#col_adj = {'PCL.Del.Pcs.N':'PCL Pcs','PCL.Del.Stops.N':'Stops','Agent.Del.Pcs.N':'Agent Pcs','Total.Del.Stops.N':'Stops'}
+#adjusting_variables = ['PCL.Del.Pcs.N', 'PCL.Del.Stops.N','PCL.PU.Pcs.N', 'PCL.PU.Stops.N',
+#                       'Agent.Del.Pcs.N', 'Agent.PU.Pcs.N','Total.Del.Stops.N', 'Total.PU.Stops.N']
+adjusting_variables = {'PCL.Del.Pcs.N','PCL.Del.Stops.N','Agent.Del.Pcs.N','Total.Del.Stops.N'}
 
 
-df_NEW = df
+db_peak     =  df.query("`Week` in (47,48,49,50,51,52) and `Year`==2024")
+db_non_peak =  df.query("`Week` not in (47,48,49,50,51,52) or `Year`!=2024")
+
 for var in adjusting_variables:
     
     dis =  distributions.query(" `Week` in (47,48,49,50,51,52)")
     
     print(var)
     #i = 'PCL.Del.Pcs.N'
-    dff_r = df[['Terminal','Year','Week'] + [var]].query("`Week` not in (47,48,49,50,51,52)")
-    dff_r.columns = ['Terminal','Year','Week']+[var]
-    dff = df[['Terminal','Year','Week'] + [var]].query("`Week` in (47,48,49,50,51,52)")
-    dff.columns = ['Terminal','Year','Week']+[var]
-    dff_groupby = pd.DataFrame(dff.groupby(['Year','Terminal'])[var].sum()).reset_index()
-    dff_groupby.columns = ['Year','Terminal','Total']
-    dff = pd.merge(dff,dff_groupby[['Year','Terminal','Total']])
+    df_non_peak = df[['Terminal','Year','Week'] + [var]].query("`Week` not in (47,48,49,50,51,52) or `Year`!=2024")
+    df_non_peak.columns = ['Terminal','Year','Week']+[var]
+    df_peak = df[['Terminal','Year','Week'] + [var]].query("`Week` in (47,48,49,50,51,52) and `Year`==2024")
+    df_peak.columns = ['Terminal','Year','Week']+[var]
+    df_peak_groupby = pd.DataFrame(df_peak.groupby(['Year','Terminal'])[var].sum()).reset_index()
+    df_peak_groupby.columns = ['Year','Terminal','Total']
+    df_peak = pd.merge(df_peak,df_peak_groupby[['Year','Terminal','Total']])
     
     dis = dis[['Year','Terminal','Week'] + [var]]
     dis_groupby = dis.groupby(['Year','Terminal'])[var].sum().reset_index()
@@ -56,21 +60,28 @@ for var in adjusting_variables:
     dis['dis'] = dis[var+"_x"]/dis[var+"_y"]
     dis.columns =['Year', 'Terminal','Week']+[var]+['Total', 'dis']
     
-    dfg = pd.merge(dff, dis[['Year','Week','Terminal','dis']],on=['Year','Week','Terminal'])
-    dfg['New_value'] = dfg['dis']*dfg['Total']
-    #df_final = pd.merge(dfg,dis[['Year','Week', 'Terminal']+[var]],on=['Year','Week', 'Terminal'])
-    #df_final = pd.concat([df_final,dff_r],axis=0)
-    df_final = dfg.sort_values(by=['Terminal','Week','Year'])
+    df_peak_new = pd.merge(df_peak, dis[['Year','Week','Terminal','dis']],on=['Year','Week','Terminal'])
+    df_peak_new['New_value'] = df_peak_new['dis']*df_peak_new['Total']
+    #df_final = pd.merge(df_peak_new,dis[['Year','Week', 'Terminal']+[var]],on=['Year','Week', 'Terminal'])
+    #df_final = pd.concat([df_final,df_non_peak],axis=0)
+    df_peak_new = df_peak_new.sort_values(by=['Terminal','Week','Year'])
     
     #df_final.loc[df_final['New_value'].isnull(),'New_value'] = df_final.loc[df_final['New_value'].isnull(),var]
-    df_NEW = pd.merge(df_NEW,df_final[['Terminal', 'Year', 'Week', 'New_value']],on=['Terminal', 'Year', 'Week'])  
-    df_NEW = df_NEW.drop(var,axis=1)
-    df_NEW = df_NEW.rename(columns={'New_value':var+'_EDITED'})
+    db_peak = pd.merge(db_peak,df_peak_new[['Terminal', 'Year', 'Week', 'New_value']],on=['Terminal', 'Year', 'Week'])  
+    db_peak['New_value'] = db_peak['New_value'].fillna(0)
+    db_peak = db_peak.drop(var,axis=1)
+    db_peak = db_peak.rename(columns={'New_value':var+'_EDITED'})
 
 
-df_output = df_NEW[['Division', 'District', 'Terminal', 'Terminal.Name', 'Province', 'Year','Quarter', 'Period.Number', 'Week',
+db_peak = db_peak[['Division', 'District', 'Terminal', 'Terminal.Name', 'Province', 'Year','Quarter', 'Period.Number', 'Week',
                     'PCL.Del.Pcs.N_EDITED','PCL.Del.Stops.N_EDITED','PCL.PU.Pcs.N', 'PCL.PU.Stops.N', 'Agent.Del.Pcs.N_EDITED',
                     'Agent.PU.Pcs.N','Total.Del.Stops.N_EDITED', 'Total.PU.Stops.N']]
-df_output.to_excel(r'C:\My Folder\P&D Forecast\P&D Forecast v2\Excel Output\ForecastResults_onesheet_edited_usingPython.xlsx')
+
+db_non_peak.columns = db_peak.columns
+
+finalized_db = pd.concat([db_non_peak,db_peak],axis=0)
+finalized_db = finalized_db.sort_values(by=['Terminal','Year','Week'])
+
+finalized_db.to_excel(r'C:\My Folder\P&D Forecast\P&D Forecast v2\Excel Output\ForecastResults_onesheet_edited_usingPython_version.xlsx')
 
 #df.to_excel(r'C:\My Folder\P&D Forecast\P&D Forecast v2\Excel Output\ForecastResults_onesheet_modified.xlsx')

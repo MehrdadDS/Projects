12b0ce6e29b63403ecdd96fbf690aa0928a44177
Mehrdad <mehrdad.dadgar@gmail.com>
2024-07-19 17:34:46 -0400

    Adding plots to the code to show better adding flexibility to adjust all of variables

--- Forecast Adjustments/Changing Distribution/changing_distribution.py
+++ Forecast Adjustments/Changing Distribution/changing_distribution.py
@@ -12,18 +12,22 @@ from datetime import datetime,timedelta
 import numpy as np
 import datetime
 from datetime import timedelta
-
-
+from ops_plots import plot_trendlines
 ########################################## Import files
 # Import Forecast
 
 df = pd.read_excel('C:\My Folder\P&D Forecast\P&D Forecast v2\Excel Output\ForecastResults_onesheet_modified.xlsx')
-distributions = pd.read_excel('C:\My Folder\P&D Forecast\P&D Forecast v2\Excel Output\May 7\ForecastResults-Check- April 25th- Checking 2024.xlsm',sheet_name="Actual")
-
-
+distributions = pd.read_excel('C:\My Folder\P&D Forecast\P&D Forecast v2\Excel Output\ForecastResults-Check- July 19th.xlsm',sheet_name="Actual")
+actuals = pd.read_excel(r'C:\My Folder\P&D Forecast\P&D Forecast v2\Excel Output\Actual.xlsx')
 df.columns = ['Division', 'District', 'Terminal', 'Terminal.Name', 'Province', 'Year','Quarter', 'Period.Number','Week'
               , 'PCL.Del.Pcs.N','PCL.Del.Stops.N', 'PCL.PU.Pcs.N', 'PCL.PU.Stops.N', 'Agent.Del.Pcs.N',
               'Agent.PU.Pcs.N', 'Total.Del.Stops.N', 'Total.PU.Stops.N']
+actuals.columns = df.columns
+actuals = actuals[actuals['Year']>=2022]
+current_year = 2024
+last_week_actual = 28
+db_plots = pd.concat([df,actuals],axis=0)
+
 
  
 distributions = distributions.iloc[:,:17]
@@ -34,21 +38,23 @@ distributions.columns = df.columns
 distributions = distributions[distributions['Year']==2024]
 #adjusting_variables = ['PCL.Del.Pcs.N', 'PCL.Del.Stops.N','PCL.PU.Pcs.N', 'PCL.PU.Stops.N',
 #                       'Agent.Del.Pcs.N', 'Agent.PU.Pcs.N','Total.Del.Stops.N', 'Total.PU.Stops.N']
-adjusting_variables = {'PCL.Del.Pcs.N','PCL.Del.Stops.N','Agent.Del.Pcs.N','Total.Del.Stops.N'}
+adjusting_variables = ['PCL.Del.Pcs.N','PCL.Del.Stops.N', 'PCL.PU.Pcs.N',
+                        'PCL.PU.Stops.N', 'Agent.Del.Pcs.N','Agent.PU.Pcs.N',
+                        'Total.Del.Stops.N', 'Total.PU.Stops.N']
+redistributed_weeks = [43,44,45,46,47,48,49,50,51,52]
 
-
-db_peak     =  df.query("`Week` in (47,48,49,50,51,52) and `Year`==2024")
-db_non_peak =  df.query("`Week` not in (47,48,49,50,51,52) or `Year`!=2024")
+db_peak     =  df.query(f"`Week` in {redistributed_weeks} and `Year`==2024")
+db_non_peak =  df.query(f"`Week` not in {redistributed_weeks} or `Year`!=2024")
 
 for var in adjusting_variables:
     
-    dis =  distributions.query(" `Week` in (47,48,49,50,51,52)")
+    dis =  distributions.query(f" `Week` in {redistributed_weeks}")
     
     print(var)
     #i = 'PCL.Del.Pcs.N'
-    df_non_peak = df[['Terminal','Year','Week'] + [var]].query("`Week` not in (47,48,49,50,51,52) or `Year`!=2024")
+    df_non_peak = df[['Terminal','Year','Week'] + [var]].query(f"`Week` not in {redistributed_weeks} or `Year`!=2024")
     df_non_peak.columns = ['Terminal','Year','Week']+[var]
-    df_peak = df[['Terminal','Year','Week'] + [var]].query("`Week` in (47,48,49,50,51,52) and `Year`==2024")
+    df_peak = df[['Terminal','Year','Week'] + [var]].query(f"`Week` in {redistributed_weeks} and `Year`==2024")
     df_peak.columns = ['Terminal','Year','Week']+[var]
     df_peak_groupby = pd.DataFrame(df_peak.groupby(['Year','Terminal'])[var].sum()).reset_index()
     df_peak_groupby.columns = ['Year','Terminal','Total']
@@ -69,19 +75,21 @@ for var in adjusting_variables:
     #df_final.loc[df_final['New_value'].isnull(),'New_value'] = df_final.loc[df_final['New_value'].isnull(),var]
     db_peak = pd.merge(db_peak,df_peak_new[['Terminal', 'Year', 'Week', 'New_value']],on=['Terminal', 'Year', 'Week'])  
     db_peak['New_value'] = db_peak['New_value'].fillna(0)
-    db_peak = db_peak.drop(var,axis=1)
+    #db_peak = db_peak.drop(var,axis=1)
     db_peak = db_peak.rename(columns={'New_value':var+'_EDITED'})
 
-
 db_peak = db_peak[['Division', 'District', 'Terminal', 'Terminal.Name', 'Province', 'Year','Quarter', 'Period.Number', 'Week',
-                    'PCL.Del.Pcs.N_EDITED','PCL.Del.Stops.N_EDITED','PCL.PU.Pcs.N', 'PCL.PU.Stops.N', 'Agent.Del.Pcs.N_EDITED',
-                    'Agent.PU.Pcs.N','Total.Del.Stops.N_EDITED', 'Total.PU.Stops.N']]
+                    'PCL.Del.Pcs.N_EDITED','PCL.Del.Stops.N_EDITED','PCL.PU.Pcs.N_EDITED', 'PCL.PU.Stops.N_EDITED', 'Agent.Del.Pcs.N_EDITED',
+                    'Agent.PU.Pcs.N_EDITED','Total.Del.Stops.N_EDITED', 'Total.PU.Stops.N_EDITED']]
+
+
+plot_trendlines(db_plots, db_peak, adjusting_variables,current_year=2024)
 
 db_non_peak.columns = db_peak.columns
 
 finalized_db = pd.concat([db_non_peak,db_peak],axis=0)
 finalized_db = finalized_db.sort_values(by=['Terminal','Year','Week'])
 
-finalized_db.to_excel(r'C:\My Folder\P&D Forecast\P&D Forecast v2\Excel Output\ForecastResults_onesheet_edited_usingPython_version.xlsx')
+finalized_db.to_excel(r'C:\My Folder\P&D Forecast\P&D Forecast v2\Excel Output\ForecastResults_onesheet_redistributed_usingPython.xlsx')
 
 #df.to_excel(r'C:\My Folder\P&D Forecast\P&D Forecast v2\Excel Output\ForecastResults_onesheet_modified.xlsx')
